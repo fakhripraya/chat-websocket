5643cef7090b066a286eca76b14a538b3b59d125
fakhripraya <fakhri@indomaret.co.id>
2021-05-30 13:57:39 +0700

    adding database connection for chats

---
+++ models/room_chat.js
@@ -0,0 +1,150 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const db = require('../config/database');
+
+const DBChatRoom = db.define('db_chat_rooms', {
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    room_desc: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    is_active: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+    },
+    created_by: {
+        type: DataTypes.STRING
+    },
+    modified: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+    },
+    modified_by: {
+        type: DataTypes.STRING
+    }
+}, {
+    // don't add the timestamp attributes (updatedAt, createdAt)
+    timestamps: false,
+    // your other configuration here
+});
+
+const DBChatRoomMembers = db.define('db_chat_room_members', {
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    room_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    },
+    socket_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    },
+    is_active: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+    },
+    created_by: {
+        type: DataTypes.STRING
+    },
+    modified: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+    },
+    modified_by: {
+        type: DataTypes.STRING
+    }
+}, {
+    // don't add the timestamp attributes (updatedAt, createdAt)
+    timestamps: false,
+    // your other configuration here
+});
+
+const DBChatRoomChats = db.define('db_chat_room_chats', {
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    room_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    },
+    sender_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    },
+    chat_body: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    attachment: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    pic_url: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    is_active: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+    },
+    created_by: {
+        type: DataTypes.STRING
+    },
+    modified: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+    },
+    modified_by: {
+        type: DataTypes.STRING
+    }
+}, {
+    // don't add the timestamp attributes (updatedAt, createdAt)
+    timestamps: false,
+    // your other configuration here
+});
+
+DBChatRoom.sync().then(() => {
+    console.log('DBChatRoom table created');
+});
+
+DBChatRoomMembers.sync().then(() => {
+    console.log('DBChatRoomMembers table created');
+});
+
+DBChatRoomChats.sync().then(() => {
+    console.log('DBChatRoomChats table created');
+});
+module.exports = { DBChatRoom, DBChatRoomMembers, DBChatRoomChats };
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const app = require('express')()
+const { Op } = require("sequelize");
+const cors = require('cors');
 const http = require('http').createServer(app)
 const io = require('socket.io').listen(http, {
     "transports": ['websocket',
@@ -10,10 +12,114 @@ const io = require('socket.io').listen(http, {
     "polling duration": 10
 });
 
+// Database
+const db = require('./config/database');
+
+const { DBChatRoom, DBChatRoomMembers, DBChatRoomChats } = require('./models/room_chat');
+
+app.use(cors());
+
+// Test DB
+db.authenticate()
+    .then(() => console.log('Connection to the database is established'))
+    .catch(err => console.log('Error: ' + err))
+
 io.on('connection', socket => {
 
-    socket.on('send message', ({ message, type, senderId }) => {
-        io.emit('set message', { message, type, senderId })
+    socket.on('join room', (room, otherUser, callback) => {
+
+        DBChatRoomMembers.findAll({
+            where: {
+                room_id: {
+                    [Op.eq]: room.id
+                }
+            }
+        })
+            .then(function (res) {
+                console.log(res)
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+
+        // db.transaction().then(function (t) {
+        //     DBChatRoom.create({
+        //         room_desc: "room-" + socket.id.toString(),
+        //         is_active: true,
+        //         created: user.displayname,
+        //         modified: user.displayname,
+        //     }, {
+        //         transaction: t
+        //     }).then(function (res) {
+
+        //         console.log(res)
+
+        //         DBChatRoomMembers.create({
+        //             room_id: res.id,
+        //             user_id: user.id,
+        //             socket_id: socket.id,
+        //             is_active: true,
+        //             created: name,
+        //             modified: name,
+        //         }, {
+        //             transaction: t
+        //         }).then(function () {
+
+        //             socket.join(res.room_desc);
+        //             t.commit();
+
+        //             callback({ error: null, user: { id: socket.id, name: user.displayname, room: "room-" + socket.id.toString() } });
+
+        //         }).catch(function (error) {
+
+        //             t.rollback();
+        //             callback({ error: error, user: null });
+
+        //         });
+
+        //     }).catch(function (error) {
+
+        //         t.rollback();
+        //         callback({ error: error, user: null });
+
+        //     });
+        // });
+    });
+
+    socket.on('send message', ({ message, type, senderId, room }) => {
+
+        db.transaction().then(function (t) {
+            DBChatRoomChats.create({
+                is_active: true,
+                created: name,
+                modified: name,
+            }, {
+                transaction: t
+            }).then(function () {
+
+                io.to(room).emit('set message', { message, type, senderId })
+
+                t.commit();
+
+                callback({ error: null, user: user });
+
+            }).catch(function (error) {
+
+                io.to(room).emit('get error', { message: error.message, type, senderId })
+
+                t.rollback();
+
+            });
+        });
+    })
+
+    socket.on('disconnect', () => {
+        // const user = removeUser(socket.id);
+
+        // if (user) {
+        //     io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+        //     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+        // }
     })
 
 })
