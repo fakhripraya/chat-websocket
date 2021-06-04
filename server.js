52407e5172794174a7debc54d7dd86a5bad10cdc
fakhripraya <fakhri@indomaret.co.id>
2021-06-04 21:30:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('express')()
-const { Op } = require("sequelize");
+const { Op, where } = require("sequelize");
 const cors = require('cors');
 const http = require('http').createServer(app)
 const io = require('socket.io').listen(http, {
@@ -31,89 +31,90 @@ io.on('connection', socket => {
 
     socket.on('join room', (room, user, users, callback) => {
 
-        DBChatRoomMembers.findAll({
+        // trigger rerender to the selected user for joining the room
+        io.to(socket.id).emit('trigger' + user.id)
+
+        DBChatRoomChats.findAll({
             raw: true,
             where: {
                 room_id: {
                     [Op.eq]: room.id
                 }
             }
-        })
-            .then(function (res) {
-                if (res !== null && res.length !== 0) {
+        }).then(function (res) {
 
-                    DBChatRoomChats.findAll({
-                        raw: true,
+            if (res !== null && res.length !== 0) {
+
+                DBChatRoomChats.update(
+                    {
+                        chat_read: true
+                    },
+                    {
                         where: {
+                            sender_id: {
+                                [Op.ne]: user.id
+                            },
                             room_id: {
                                 [Op.eq]: room.id
                             }
                         }
-                    }).then(function (res4) {
-
-                        socket.join(room.room_desc);
-                        callback({ error: null, callbackUsers: res, callbackRoom: room, callbackChats: res4 });
-
-                    }).catch(function (err4) {
-                        callback({ error: err4, callbackUsers: null, callbackRoom: null, callbackChats: null });
-                    })
-
-                } else {
-                    if (users !== null) {
-                        db.transaction().then(function (t) {
-                            DBChatRoom.create({
-                                room_desc: "room-" + socket.id.toString(),
-                                is_active: true,
-                                created_by: user.displayname,
-                                modified_by: user.displayname,
-                            }, {
-                                transaction: t
-                            }).then(function (res2) {
-
-                                const roomObj = res2.get({ plain: true })
-
-                                users.forEach(u => {
-                                    DBChatRoomMembers.create({
-                                        room_id: roomObj.id,
-                                        user_id: u.id,
-                                        is_active: true,
-                                        created_by: user.displayname,
-                                        modified_by: user.displayname,
-                                    }, {
-                                        transaction: t
-                                    }).then(function (res3) {
-
-                                        const userObjs = res3.get({ plain: true })
-
-                                        socket.join(roomObj.room_desc);
-                                        t.commit();
-
-                                        callback({ error: null, callbackUsers: userObjs, callbackRoom: roomObj, callbackChats: null });
-
-                                    }).catch(function (err3) {
-
-                                        t.rollback();
-                                        callback({ error: err3, callbackUsers: null, callbackRoom: null, callbackChats: null });
-
-                                    });
-                                });
-
-                            }).catch(function (err2) {
-
-                                t.rollback();
-                                callback({ error: err2, callbackUsers: null, callbackRoom: null, callbackChats: null });
-
-                            });
-                        });
                     }
+                )
+
+                callback({ error: null, callbackRoom: room, callbackChats: res });
+
+            } else {
+
+                if (users !== null) {
+                    db.transaction().then(function (t) {
+                        DBChatRoom.create({
+                            room_desc: "room-" + socket.id.toString(),
+                            is_active: true,
+                            created_by: user.displayname,
+                            modified_by: user.displayname,
+                        }, {
+                            transaction: t
+                        }).then(function (res2) {
+
+                            const roomObj = res2.get({ plain: true })
+
+                            users.forEach(u => {
+                                DBChatRoomMembers.create({
+                                    room_id: roomObj.id,
+                                    user_id: u.id,
+                                    is_active: true,
+                                    created_by: user.displayname,
+                                    modified_by: user.displayname,
+                                }, {
+                                    transaction: t
+                                }).then(function () {
+
+                                    t.commit();
+                                    callback({ error: null, callbackRoom: roomObj, callbackChats: null });
+
+                                }).catch(function (err3) {
+
+                                    t.rollback();
+                                    callback({ error: err3, callbackRoom: null, callbackChats: null });
+
+                                });
+                            });
+
+                        }).catch(function (err2) {
+                            t.rollback();
+                            callback({ error: err2, callbackRoom: null, callbackChats: null });
+                        });
+                    });
                 }
-            })
-            .catch(function (err) {
-                callback({ error: err, callbackUsers: null, callbackRoom: null, callbackChats: null });
-            })
+
+            }
+
+        }).catch(function (err) {
+            callback({ error: err, callbackRoom: null, callbackChats: null });
+        })
     });
 
-    socket.on('send message', ({ type, sender, receiver, message, room }) => {
+    socket.on('send message', ({ type, sender, receiver, message, messages, room }) => {
 
         db.transaction().then(function (t) {
             DBChatRoomChats.create({
@@ -131,23 +132,29 @@ io.on('connection', socket => {
 
             }).then(function (res) {
 
-                io.to(room.room_desc).emit('set message', { message: res, type, senderId: sender.id })
+                console.log(receiver.user_id)
+                io.emit("set message" + receiver.user_id, { message: res, messages: messages, type, senderId: sender.id })
+                io.emit("set message" + sender.id, { message: res, messages: messages, type, senderId: sender.id })
                 t.commit();
 
                 io.emit('trigger' + sender.id)
-                io.emit('trigger' + receiver.id)
+                io.emit('trigger' + receiver.user_id)
 
             }).catch(function (error) {
 
                 console.log(error)
 
                 // io.to(room).emit('get error', { message: error.message, type, senderId })
-
                 t.rollback();
 
             });
         });
     })
+
+    socket.on('disconnect', function (reason) {
+        if (reason === "ping timeout") {
+        }
+    });
 
 })
 
